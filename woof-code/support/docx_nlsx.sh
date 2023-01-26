f0dc2a457eba3d3d78305da3f3fa039e68b56fc4
Linux-is-king <121989185+Linux-is-king@users.noreply.github.com>
2023-01-26 02:37:13 -1000

    Update docx_nlsx.sh

--- woof-code/support/docx_nlsx.sh
+++ woof-code/support/docx_nlsx.sh
@@ -14,8 +14,8 @@ if [ ! "$BUILDSYS" ] ; then
 fi
 
 if [ "$BUILD_DOCX" = "yes" -o "$BUILD_NLSX" = "yes" ] ; then
-	INSTALLED_PKGS=$(cat ../status/findpkgs_FINAL_PKGS-${DISTRO_BINARY_COMPAT}-${DISTRO_COMPAT_VERSION} | \
-cut -f 1 -d '|' | tr ':' '\n' | sed '/^$/d' | sort -u)
+	INSTALLED_PKGS=$((echo "$PETBUILDS" | tr ' ' '\n'; cat ../status/findpkgs_FINAL_PKGS-${DISTRO_BINARY_COMPAT}-${DISTRO_COMPAT_VERSION} | \
+cut -f 1 -d '|' | tr ':' '\n' | sed '/^$/d') | sort -u)
 fi
 
 if [ "$BUILD_DOCX" = "yes" ] ; then
@@ -26,29 +26,48 @@ if [ "$BUILD_DOCX" = "yes" ] ; then
 	for i in $INSTALLED_PKGS
 	do
 		if [ -d ../packages-${DISTRO_FILE_PREFIX}/${i}_DOC ] ; then
+			echo -n " ${i}"
 			cp -a --remove-destination ../packages-${DISTRO_FILE_PREFIX}/${i}_DOC/* docx/
 		fi
 	done
-	sync
+	if [ -d bdrv_DOC ] ; then
+		echo -n " bdrv_DOC"
+		cp -a --remove-destination bdrv_DOC/* docx/
+	fi
+	echo
 	rm -f docx/pet.specs
+	find docx/usr/share/doc -iname 'changelog*.gz' -delete
+	[ "$USR_SYMLINKS" = "yes" ] && usrmerge docx 0
 	echo "Creating $DOCXSFS..."
+	[ -d docx/root ] && busybox chmod 700 docx/root
+	[ -d docx/home/spot ] && busybox chmod 700 docx/home/spot
 	mksquashfs docx ${DOCXSFS} ${SFSCOMP}
 fi
 
 if [ "$BUILD_NLSX" = "yes" ] ; then
 	echo
 	rm -rf nlsx ${NLSXSFS}
-	mkdir -p docx
+	mkdir -p nlsx
 	echo "Building nlsx..."
 	for i in $INSTALLED_PKGS
 	do
 		if [ -d ../packages-${DISTRO_FILE_PREFIX}/${i}_NLS ] ; then
+			echo -n " ${i}"
 			cp -a --remove-destination ../packages-${DISTRO_FILE_PREFIX}/${i}_NLS/* nlsx/
 		fi
 	done
-	sync
+	if [ -d bdrv_NLS ] ; then
+		echo -n " bdrv_NLS"
+		cp -a --remove-destination bdrv_NLS/* nlsx/
+	fi
+	echo
 	rm -f nlsx/pet.specs
+	mkdir -p nlsx/var/local
+	touch nlsx/var/local/nlsx_loaded
+	[ "$USR_SYMLINKS" = "yes" ] && usrmerge nlsx 0
 	echo "Creating $NLSXSFS..."
+	[ -d nlsx/root ] && busybox chmod 700 nlsx/root
+	[ -d nlsx/home/spot ] && busybox chmod 700 nlsx/home/spot
 	mksquashfs nlsx ${NLSXSFS} ${SFSCOMP}
 fi
 
