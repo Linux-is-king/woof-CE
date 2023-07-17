893d2a5400919bc0b2a5e394e983e9971cee2ffb
Linux-is-king <121989185+Linux-is-king@users.noreply.github.com>
2023-07-16 19:34:11 -1000

    rolll back agian
    ..

--- woof-code/rootfs-petbuilds/spacefm/pinstall.sh
+++
@@ -1,4 +0,0 @@
-echo '#!/bin/sh
-exec spacefm "$@"' > usr/local/bin/defaultfilemanager
-
-echo none > etc/desktop_app
--- woof-code/rootfs-petbuilds/viewnior/pinstall.sh
+++
@@ -1,3 +0,0 @@
-echo '#!/bin/sh
-exec viewnior "$@"' > usr/local/bin/defaultimageviewer
-chmod 755 usr/local/bin/defaultimageviewer
